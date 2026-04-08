609b683c9191f38cec0bd7a8c11bb99436099f9b
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-04-08 13:03:13 +0100

    update

--- content.js
+++ content.js
@@ -27,11 +27,17 @@ style.textContent = `
 
 document.head.appendChild(style);
 
-const url = "https://poetrydb.org/linecount,random/3;1";
 let currentSonnetBlocker = null;
 const muteButton = document.querySelector('.ytp-volume-icon');
 
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
 const getSonnet = async () => {
+  const url = `https://poetrydb.org/linecount,random/${getRandomInt(3,15)};1`;
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -68,7 +74,6 @@ const removeBlocker = () => {
 const adWatcher = () => {
   console.log("poeticAd monitoring started");
 
-  // const skipButton = document.querySelector(".ytp-ad-skip-button-modern, .ytp-skip-ad-button, .ytp-ad-skip-button");
   const videoElement = document.querySelector("video");
   const isAdPlaying = document.querySelector('.ad-showing, .ad-interrupting');
 
@@ -97,3 +102,9 @@ const adWatcher = () => {
 }
 
 setInterval(adWatcher, 1000)
+
+
+// TODO add itv x blocker:
+//    video player: data-testid="video-wrapper" class="videoPlayerWrapper visible"
+//    advert: class="advertPlayerA advertPlayerB"
+//    < button class="cp_button cp_button--primary cp_button--small fe-mrphs__volume__button fe-mrphs__button fe-mrphs__button-iconOnly" tabindex = "4" data - testid="volumeButton" aria - label="Volume" aria - disabled="false" > <i class="cp_icon cp_icon__volume-mid cp_icon--base" aria-hidden="true"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg" role="img">    <path fill-rule="evenodd" clip-rule="evenodd" d="M11 1.00001C11 0.611994 10.7755 0.259002 10.4242 0.0944227C10.0728 -0.0701571 9.6579 -0.0166118 9.35982 0.231791L4.83797 4H1C0.447715 4 0 4.44772 0 5V12C0 12.5523 0.447715 13 1 13H4.83793L9.35982 16.7682C9.6579 17.0166 10.0728 17.0702 10.4242 16.9056C10.7755 16.741 11 16.388 11 16V1.00001ZM5.8402 5.76822L9 3.13506V13.865L5.84016 11.2318C5.66045 11.082 5.43392 11 5.19998 11H2V6H5.20002C5.43396 6 5.66049 5.91798 5.8402 5.76822Z" fill="currentColor"></path>    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.4616 4.1573C13.927 3.85996 14.5454 3.9962 14.8427 4.46161C16.2152 6.60984 16.3774 9.35259 15.2732 11.657L14.9018 12.4321C14.6632 12.9302 14.0659 13.1405 13.5679 12.9018C13.0698 12.6632 12.8595 12.0659 13.0982 11.5679L13.4696 10.7928C14.2765 9.10882 14.1552 7.10041 13.1573 5.53839C12.86 5.07298 12.9962 4.45464 13.4616 4.1573Z" fill="currentColor"></path></svg></i></button >
--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-const url = "https://poetrydb.org/linecount,random/3;1";
+const url = `https://poetrydb.org/linecount,random/${getRandomInt(3,15)};1`
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
 
 fetch(url).then(response => response.json()).then(data => console.log(data[0]))
